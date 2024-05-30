ae8dedb0f1a233eccafdbf2c3dcd045b837038e9
AD120 <avrahaaam@gmail.com>
2024-05-30 16:05:52 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,14 +11,14 @@ function setup() {
   input = createInput();
   input.position(150, 80);
 
-  button = createButton('בדוק תשובה');
+  button = createButton("שאל את גארי");
   button.position(150, 120);
   button.mousePressed(checkAnswer);
 
-  message = createElement('h2', '');
+  message = createElement("h2", "");
   message.position(150, 150);
 
-  newGameButton = createButton('משחק חדש');
+  newGameButton = createButton("משחק חדש");
   newGameButton.position(150, 200);
   newGameButton.hide();
   newGameButton.mousePressed(newGame);
@@ -31,9 +31,9 @@ function draw() {
   text(`${num1} * ${num2} = ?`, width / 2, height / 2 - 50);
   textSize(24);
   text(`ניקוד: ${score}`, width / 2, height / 2 + 50);
-  
-  // הדפסת הזמן החולף ב-console
-  console.log(`זמן המשחק החולף: ${(millis() - startTime) / 1000} שניות`);
+
+  // // הדפסת הזמן החולף ב-console
+  // console.log(`זמן המשחק החולף: ${(millis() - startTime) / 1000} שניות`);
 
   // בדיקה אם זמן המשחק עבר את 90 שניות
   if (!gameOverFlag && millis() - startTime > 90000) {
@@ -42,30 +42,38 @@ function draw() {
 }
 
 function generateQuestion() {
-  num1 = int(random(1, 10));
-  num2 = int(random(1, 10));
+  num1 = int(random(2, 11));
+  num2 = int(random(2, 11));
 }
 
 function checkAnswer() {
+  textAlign(CENTER, CENTER);
   userAnswer = int(input.value());
   if (userAnswer === num1 * num2) {
-    message.html('הצלחת יא גארי מלך!');
+    message.html("הצלחת יא גארי מלך!");
     score++;
-    generateQuestion();
+   setTimeout(() => {
+      message.html('');
+      generateQuestion();
+    }, 1000);
   } else {
-    message.html(`טעית! גארי יתפוס אותך ${num1 * num2}`);
+    let correctAnswer = num1 * num2;
+    message.html(`טעית! גארי יתפוס אותך \n${correctAnswer}`);
     score--;
-    setTimeout(generateQuestion, 2000); // מחכה 2 שניות לפני הצגת השאלה הבאה
+    setTimeout(() => {
+      message.html("");
+      generateQuestion();
+    }, 2000); // מחכה 2.5 שניות לפני שהודעת השגיאה נמחקת
   }
-  input.value('');
+  input.value("");
 }
 
 function gameOver() {
+  textAlign(CENTER, CENTER);
   noLoop(); // עצירת המשחק
   gameOverFlag = true;
   textSize(48);
-  textAlign(CENTER, CENTER);
-  text('המשחק נגמר!', width / 2, height / 2);
+  text("המשחק נגמר!", width / 2, height / 2);
   message.html(`הניקוד שלך: ${score}`);
   newGameButton.show();
 }
@@ -77,4 +85,10 @@ function newGame() {
   generateQuestion();
   loop(); // התחלת המשחק מחדש
   newGameButton.hide();
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    checkAnswer();
+  }
 }
